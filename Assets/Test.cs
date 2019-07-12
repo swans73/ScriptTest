e9174fcf392fbfe5efa42b7bb661da71eb6d0f9d
白鳥雅人 <whitesplash0703@icloud.com>
2019-07-13 05:35:29 +0900

    Lesson4の課題
    配列を宣言して出力、クラスに関数を作成し呼び出しました。

---
+++ Assets/Test.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+	public class Boss {
+		public int mp = 53;
+		public void Magic() {
+			if(mp > 5) {
+				Debug.Log("魔法攻撃をした。残りmpは" + this.mp );
+				this.mp -= 5;
+			} else {
+				Debug.Log("MPが足りないため魔法が使えない。");
+			}
+		}
+	}
+	
+	// Use this for initialization
+	void Start () {
+		int[] array = {11, 22, 33, 44, 55};
+		for(int i = 0; i < array.Length; i++)
+			Debug.Log(array[i]);
+
+		for(int p = 4; p >= 0; p--)
+			Debug.Log(array[p]);
+		
+		Boss boss = new Boss ();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		boss.Magic();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
